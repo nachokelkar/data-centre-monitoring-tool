a372b059d24197a83bfd405b11fcc79802072b38
Rishi Ravikumar <rishiravi.k98@gmail.com>
2020-04-08 19:00:19 +0530

    Get hbase data api

--- flaskapp.py
+++ flaskapp.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, request, json, url_for, send_file
-import ecks
+# import ecks
 import subprocess
+import happybase
+from datetime import datetime
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
+
 
 @app.route('/')
 def index():
@@ -15,34 +19,61 @@ def index():
 #     ip = ip_out.find("inet")
 #     return render_template("scan.html")
 
-@app.route('/check', methods=["POST"])
-def check():
-    if request.method=="POST":
-        result=request.get_json(force=True)
-        ip = str(result[0])
-        
-        e = ecks.Ecks()
-        toflash = ""
-        tab = "&nbsp;&nbsp;&nbsp;&nbsp;"
 
-        ecksdiskdefs = {1:"Other", 2:"RAM", 3:"Virtual Memory", 4:"Fixed Disk", 5:"Removable Disk", 6:"Floppy Disk", 7:"Compact Disk", 8:"RAM Disk", 9:"Flash Memory", 10:"Network Disk"}
+# @app.route('/check', methods=["POST"])
+# def check():
+#     if request.method == "POST":
+#         result = request.get_json(force=True)
+#         ip = str(result[0])
 
-        uptime = str(e.get_data(ip, 'public', 'uptime'))
-        disk_data = e.get_data(ip, 'public', 'disk')
-        cpu_data = e.get_data(ip, 'public', 'cpu')
+#         e = ecks.Ecks()
+#         toflash = ""
+#         tab = "&nbsp;&nbsp;&nbsp;&nbsp;"
 
-        toflash += "Uptime : " +uptime +"<br>"
-        toflash += "CPU Usage: <br>" +tab +"User : " +str(cpu_data[0]) +"%<br>" +tab +"System : " +str(cpu_data[1]) +"%<br>" +tab +"Idle : " +str(cpu_data[2]) +"%<br>"
-        toflash += "Disk : <br>" +tab
-        for i in disk_data:
-            toflash += ecksdiskdefs[i[0]] +"&nbsp;&nbsp;" +i[1] +" : " +str(i[2]/2**20) +"MB TOTAL, " +str(i[3]/2**20) +"MB USED <br>" +tab
+#         ecksdiskdefs = {1: "Other", 2: "RAM", 3: "Virtual Memory", 4: "Fixed Disk", 5: "Removable Disk",
+#                         6: "Floppy Disk", 7: "Compact Disk", 8: "RAM Disk", 9: "Flash Memory", 10: "Network Disk"}
 
-        return json.dumps({"success":"True", "message":toflash}),200,{'ContentType':'application/json'}
-    
+#         uptime = str(e.get_data(ip, 'public', 'uptime'))
+#         disk_data = e.get_data(ip, 'public', 'disk')
+#         cpu_data = e.get_data(ip, 'public', 'cpu')
+
+#         toflash += "Uptime : " + uptime + "<br>"
+#         toflash += "CPU Usage: <br>" + tab + "User : " + str(cpu_data[0]) + "%<br>" + tab + "System : " + str(
+#             cpu_data[1]) + "%<br>" + tab + "Idle : " + str(cpu_data[2]) + "%<br>"
+#         toflash += "Disk : <br>" + tab
+#         for i in disk_data:
+#             toflash += ecksdiskdefs[i[0]] + "&nbsp;&nbsp;" + i[1] + " : " + str(
+#                 i[2]/2**20) + "MB TOTAL, " + str(i[3]/2**20) + "MB USED <br>" + tab
+
+#         return json.dumps({"success": "True", "message": toflash}), 200, {'ContentType': 'application/json'}
+
+
+@app.route('/test')
+def test():
+    conn = happybase.Connection('localhost', port=9090)
+    table = conn.table('snmp')
+    row_key = str(datetime.now())[:16]
+    conn.open()
+
+    # row = table.row(b'2020-04-08 18:03:13.832322')
+    # print(row[b'127.0.0.1:upt'])
+    # for (key, data) in table.scan(row_start=row_key):
+    #     print(key, data)
+    #     print()
+
+    data = [data for (key, data) in table.scan(row_start=row_key)]
+    conn.close()
+
+    print(data[-1])
+
+    return json.dumps({"data": ["<rack_no>", "<server_name>", "<ip_addr>", "<OS>", "<cpu data>", "<dsk data>"]}), 200, {'ContentType': 'application/json'}
+
+
 @app.route('/download')
 def download():
     path = "diskdata.txt"
     return send_file(path)
 
+
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
